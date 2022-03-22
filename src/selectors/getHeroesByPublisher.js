1eda704e3013deed1f7c40548f220b67566d3f2c
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-03-22 00:37:28 -0600

    HeroesQuery

--- src/selectors/getHeroesByPublisher.js
+++ src/selectors/getHeroesByPublisher.js
@@ -1,13 +1,12 @@
 import { heroes } from "../data/heroes"
 
 
-
 export const getHeroesByName = (name = '') => {
 
-    //name = name.toLowerCase();
+    if ( name.length === 0 ) {
+        return [];
+    }
 
-    //return heroes.filter(hero => hero.name.toLowerCase().includes(name));
-
-    return [...heroes];
-
+    name = name.toLowerCase();
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
 }
