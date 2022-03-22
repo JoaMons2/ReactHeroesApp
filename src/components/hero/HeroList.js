0bdce0ec13af9b06bc1c1720bd3a7b239d8e3b5f
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-03-21 23:54:34 -0600

    HeroesMemo

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,16 +1,13 @@
+import { useMemo } from 'react';
 import { HeroCard } from "./HeroCard";
 import { getHeroesByPublisher } from "../../helpers/getHeroesByPublisher"
 
 
 export const HeroList = ({ publisher }) => {
 
-    const validPublisher = ['DC Comics', 'Marvel Comics'];
 
-    if( !validPublisher.includes(publisher) ){
-        throw new Error(`${ publisher } is not a valid publisher`);
-    }
+    const heroes = useMemo(() => getHeroesByPublisher( publisher ), [ publisher ]) ;
 
-    const heroes = getHeroesByPublisher(publisher)
     return (
         <div className="row rows-cols-1 row-cols-md-3 g-3">
                 {
@@ -21,6 +18,6 @@ export const HeroList = ({ publisher }) => {
                     />
                     ))
                 }
-                </div>
+        </div>
     )
 }
