dc3151f971840fe65385bec4f544ea0a714d05ce
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-03-21 22:39:27 -0600

    Router

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -3,14 +3,14 @@ import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const handleLogout = () => {
-        // TODO: Por hacer
-        navigate('login', {
-            replace: true
-        })
-    }
+    // const handleLogout = () => {
+    //     // TODO: Por hacer
+    //     navigate('login', {
+    //         replace: true
+    //     })
+    // }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DcScreen } from '../components/dc/DcScreen';
 import { LoginScreen } from '../components/login/LoginScreen';
-import { DashboardRoutes } from './DashboardRoutes';
+//import { DashboardRoutes } from './DashboardRoutes';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { SerchScreen } from '../components/search/SearchScreen';
+
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+        <Navbar/>
         <Routes>
-            <Route path='/login' element={<LoginScreen />} />
-            <Route path='/*' element={<DashboardRoutes />} />
+            <Route path="/" element={<MarvelScreen/>} />
+            <Route path="marvel" element={<MarvelScreen />} />
+            <Route path="dc" element={<DcScreen />} />
+            <Route path="search" element={<SerchScreen/>} />
+            <Route path="login" element={<LoginScreen/>} />
         </Routes>
     </BrowserRouter>
   )
